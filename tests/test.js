5d8e70f06cc2fecd2b4cc9e859e03324d0a3c1c0
Stuart Colville <scolville@mozilla.com>
2017-06-09 20:21:00 +0100

    Add tests

---
+++ tests/test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+const eslint = require('eslint');
+
+const input = [
+  'index.js',
+  'tests/test.js',
+];
+
+const output = new eslint.CLIEngine({
+  envs: ['es6', 'node'],
+  configFile: 'index.js',
+  useEslintrc: false,
+}).executeOnFiles(input);
+
+
+input.forEach((file, index) => {
+  if (output.results[index].messages.length) {
+    // eslint-disable-next-line no-console
+    console.log(output.results[index].messages);
+  }
+  assert(output.results[index].filePath.endsWith(file));
+});
+
+assert.equal(output.errorCount, 0,
+  `Should be 0 errors but there were ${output.errorCount}`);
+assert.equal(output.warningCount, 0,
+  `Should be 0 warnings but there were ${output.warningCount}`);
