a2e77166cf8e5b44b936b451296cfa756b6f466a
William Durand <will+git@drnd.me>
2018-08-14 19:16:18 -0700

    Disable react/jsx-one-expression-per-line and react/sort-comp

--- index.js
+++ index.js
@@ -178,6 +178,9 @@ module.exports = {
     'react/prefer-stateless-function': 'off',
     'react/require-default-props': 'off',
 
+    // See: https://github.com/mozilla/eslint-config-amo/issues/63
+    'react/sort-comp': 'off',
+
     // Use a 2 space indent for jsx props.
     'react/jsx-indent-props': ['error', 2],
 
@@ -190,6 +193,9 @@ module.exports = {
     // Allow more than one prop when the jsx is confined to a single line.
     // Defaults to 1 prop per line when the jsx spans multiple lines.
     'react/jsx-max-props-per-line': ['error', { when: 'multiline' }],
+
+    // This rule conflicts with Prettier.
+    'react/jsx-one-expression-per-line': 'off',
 
     // Make sure the first prop is on a new line for multiline props.
     'react/jsx-first-prop-new-line': ['error', 'multiline'],
