95f84316d5a70453f8aa16b3a7e5450f8256ed5c
William Durand <will+git@drnd.me>
2025-10-27 17:18:37 +0100

    Remove airbnb configs (#654)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
-  // Use `airbnb`, which includes `airbnb:base` and the React-related configs.
-  extends: ['airbnb', 'amo/base'],
+  extends: [
+    'amo/base',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+  ],
   rules: {
     // Turn down warnings for our custom Link component.
     'jsx-a11y/anchor-is-valid': [
@@ -24,24 +27,9 @@ module.exports = {
       },
     ],
 
-    // Airbnb has turned this rule on again but we don't want that yet.
     'react/destructuring-assignment': ['off', 'always'],
-
-    // The airbnb default of this rule mainly encourages `shape` over `object`
-    // but there are too many bugs in the linter to use `shape` accurately.
     'react/forbid-prop-types': 'off',
 
-    'react/prefer-stateless-function': 'off',
-    'react/require-default-props': 'off',
-
-    // See: https://github.com/mozilla/eslint-config-amo/issues/63
-    'react/sort-comp': 'off',
-
-    // Use a 2 space indent for jsx props.
-    'react/jsx-indent-props': ['error', 2],
-
-    // Aligns closing brackets in jsx with the opening line.
-    'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
     'react/jsx-filename-extension': [
       'warn',
       {
@@ -49,20 +37,21 @@ module.exports = {
       },
     ],
 
-    // Allow more than one prop when the jsx is confined to a single line.
-    // Defaults to 1 prop per line when the jsx spans multiple lines.
-    'react/jsx-max-props-per-line': ['error', { when: 'multiline' }],
-
     // Make sure the first prop is on a new line for multiline props.
     'react/jsx-first-prop-new-line': ['error', 'multiline'],
+
     'react/jsx-key': 'error',
+    'react/jsx-props-no-spreading': 'off',
+
     'react/no-string-refs': 'error',
+    'react/prefer-stateless-function': 'off',
+    'react/require-default-props': 'off',
+
+    // See: https://github.com/mozilla/eslint-config-amo/issues/63
+    'react/sort-comp': 'off',
 
     // Enforces where React component static properties should be positioned.
     // We want them in the class body.
     'react/static-property-placement': ['error', 'static public field'],
-
-    // Airbnb enforces this but we use this feature a lot!
-    'react/jsx-props-no-spreading': 'off',
   },
 };
